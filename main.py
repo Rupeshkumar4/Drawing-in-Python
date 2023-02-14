440ed3e9e110d70efb152edb253ee6eec570c6c2
RUPESH KUMAR <98460861+Rupeshkumar4@users.noreply.github.com>
2023-02-14 21:14:19 +0530

    Add files via upload
    initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle as t
+import colorsys as cs
+
+t.setup(800,800)
+t.speed(0)
+t.width(2)
+t.bgcolor("black")
+
+for j in range(25):
+    for i in range(15):
+        t.color(cs.hls_to_rgb(i/15,j/25,0.4))
+        t.right(90)
+        t.circle(200-j*4,90)
+        t.left(90)
+        t.speed(45)
+        t.circle(200-j*4,90)
+        t.right(180)
+        t.circle(50,24)
+t.hideturtle()
+t.done()
